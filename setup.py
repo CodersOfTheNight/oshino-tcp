710b88f0ad833e15464b6a82c0b6c05c618bb6ea
Šarūnas Navickas <zaibacu@gmail.com>
2018-05-07 17:45:36 +0300

    Remove `pip.req`

--- setup.py
+++ setup.py
@@ -1,16 +1,9 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 from setuptools import setup
-from pip.req import parse_requirements
-from pip.exceptions import InstallationError
 
 from oshino_tcp.version import get_version
 
-try:
-    install_reqs = list(parse_requirements("requirements.txt", session={}))
-except InstallationError:
-    # There are no requirements
-    install_reqs = []
 
 setup(name="oshino_tcp",
       version=get_version(),
@@ -18,7 +11,7 @@ setup(name="oshino_tcp",
       author="Šarūnas Navickas",
       author_email="zaibacu@gmail.com",
       packages=["oshino_tcp"],
-      install_requires=[str(ir.req) for ir in install_reqs],
+      install_requires=["oshino"],
       test_suite="pytest",
       tests_require=["pytest", "pytest-cov"],
       setup_requires=["pytest-runner"]
